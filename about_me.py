305e9e4c257bce5f15dfef38b442d3814c9391cb
KathyLin2009 <47648693+KathyLin2009@users.noreply.github.com>
2019-08-22 20:36:52 -0400

    add the about me file

---
+++ about_me.py
@@ -0,0 +1,17 @@
+print("Hello!")
+print("Here's a picture of a dog:")
+print("o____")
+print(" |||| ")
+
+born = input("What year were you born in?")
+born = int(born)
+age = 2025 - born
+print("In the year 2025, you will be", age, "years old")
+
+
+
+person_age = input("What is your age?")
+person_age = int(person_age)
+dog_age = person_age * 7
+print("If you were a dog, you'd be", dog_age,"years old!!")
+
