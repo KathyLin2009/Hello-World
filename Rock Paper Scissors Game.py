09c7f9145f4cb6d36fbe31550d74553cff453523
KathyLin2009 <47648693+KathyLin2009@users.noreply.github.com>
2020-02-01 11:49:32 -0500

    update

--- Rock Paper Scissors Game.py
+++ Rock Paper Scissors Game.py
@@ -1,6 +1,6 @@
 import random
-moves = ["r", "p", "s"]
-player_wins = ["pr","sp", "rs"]
+moves = ["o", "___", ">8"]
+player_wins = ["___o",">8___", "o>8"]
 while True:
     player_move = input("Your move: ")
     if player_move == "q":
@@ -8,7 +8,7 @@ while True:
     computer_move = random.choice(moves)
     print("You:", player_move)
     print("Me:", computer_move)
-    if player_qmove == computer_move:
+    if player_move == computer_move:
         print("Tie!")
     elif player_move + computer_move in player_wins:
             print("You win!")
---
+++ turtle_race.py
@@ -0,0 +1,7 @@
+import turtle
+ana = turtle.Pen()
+ana.speed(0)
+ana.up()
+ana.goto(-140, 140)
+for i in range(11):
+    
